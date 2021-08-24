101977adb3bf95d206796f02cd3866579e674269
pountzas <nikos@pountzas.gr>
2021-08-24 03:23:57 +0300

    add class todolist

---
+++ src/components/TodosList.js
@@ -0,0 +1,15 @@
+class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map(todo => (
+          <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={this.props.handleChangeProps}
+        />
+        ))}
+      </ul>
+    )
+  }
+}
