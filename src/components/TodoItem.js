95f97df77dee7c780f5743f3625e7a8e8d8367ec
pountzas <nikos@pountzas.gr>
2021-08-24 03:25:48 +0300

    add arrow function todoitem

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+const TodoItem = (props) => {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={ props.todo.completed }
+        onChange={() => this.props.handleChangeProps()}
+      /> {props.todo.title}
+    </li>
+  );
+}
