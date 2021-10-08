b2017b257070a346182a918e8ab2fefbfa6deae9
pountzas <nikos@pountzas.gr>
2021-10-09 01:16:39 +0300

    add event onsubmit

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -10,9 +10,14 @@ class InputTodo extends Component {
     })
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.title);
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input
           type="text"
           placeholder="Add todo..."
